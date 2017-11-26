c7618e22398920ba6b0b984689b9b36b7d9f2910
Amanda Tsai <dingyupei24@gmail.com>
2017-11-26 15:48:39 +0800

    Update Segmentation.py

--- utilities/Segmentation.py
+++ utilities/Segmentation.py
@@ -1,38 +1,34 @@
 # -*- coding: utf8 -*-
+#!/usr/bin/env/python27
 from __future__ import print_function
 import msaf
 import librosa
-#import seaborn as sns
-
 from pydub import AudioSegment
+#import os
 
-# and IPython.display for audio output
-import IPython.display
+# Choose an audio file
+audio_file = "./Demo_songs/Chloe Riley - S.T.F.U.(instrumental).mp3"
 
-# Setup nice plots
-#sns.set(style="dark")
-#%matplotlib inline
-#import matplotlib
-
-# Choose an audio file and listen to it
-audio_file = "/Users/amandatsai/文件/NCCU/@1061/資訊專題/專題/Demo_songs/Ellie Goulding - Lights (Instrumental).mp3"
-#IPython.display.Audio(filename=audio_file)
-
-# Check which type is your input audio file
-song = AudioSegment.from_mp3(audio_file)
-#song = AudioSegment.from_wav(audio_file)
-
+# Check which type the input audio file is
+if audio_file.endswith(".mp3"):
+    song = AudioSegment.from_mp3(audio_file)
+elif audio_file.endswith("wav"):
+    song = AudioSegment.from_wav(audio_file)
+else:
+    print ("Please change your audio file.")
 
 # Segment the file using the default MSAF parameters
 boundaries, labels = msaf.process(audio_file)
 print(boundaries)
 
-# pydub does things in milliseconds(ten_seconds = 10 * 1000)
-#first_10_seconds = song[:ten_seconds]
-#last_5_seconds = song[-5000:]
+'''
+    pydub does things in milliseconds(ten_seconds = 10 * 1000)
+    first_10_seconds = song[:ten_seconds]
+    last_5_seconds = song[-5000:]
+    '''
 segments = list()
 boundaries *= 1000
-buff = 100
+buff = 50
 for index in xrange(1,len(boundaries)):
     if index == 1 or index == len(boundaries)-2 : continue
     elif index == 2 or index == len(boundaries)-1 :
@@ -45,7 +41,7 @@ for index in xrange(len(segments)):
     file_name = audio_file.split('/')[-1]
     output = file_name[:file_name.rfind('.')] + "_" + str(index+1) + file_name[file_name.rfind('.'):]
     out_format = file_name.split('.')[-1]
-    segments[index].export("OUTPUT/" + output, format = out_format)
+    segments[index].export("OUTPUT_50/" + output, format = out_format)
 
 '''
 # Sonify boundaries
