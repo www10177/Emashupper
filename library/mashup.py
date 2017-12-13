9b82c9f8227f86c026ded9f8e16a4f9504c6905b
amdsibyl <dingyupei24@gmail.com>
2017-12-13 19:12:50 +0800

    Update Volume Normalized Function (out of class)

--- library/mashup.py
+++ library/mashup.py
@@ -41,25 +41,6 @@ class mash:
     def pitch_shift(self, n_step):
         self.candSig = librosa.effects.pitch_shift(self.candSig, self.candSr, n_step)
 
-    def volume_adjust(self,input_file):
-        import subprocess
-            import shlex
-            '''
-                Command format: " ffmpeg-normalize -v <input> "
-                e.g. "ffmpeg-normalize -v ../wav/'Shape Of You (Instrumental)_2.wav'"
-                File will be named by " normalized-<input> "
-                e.g. "normalized-Shape Of You (Instrumental)_2.wav"
-                '''
-            
-            FFMPEG_CMD = "ffmpeg-normalize"
-            #input_path = "../wav/"
-            #input_file = "'Ed Sheeran - Shape Of You (Instrumental)_4.wav'"
-            #cmd = FFMPEG_CMD + ' -v ' + input_path + input_file
-            cmd = FFMPEG_CMD + ' -v ' + input_file
-            #cmd = "ffmpeg-normalize -v ../wav/'Ed Sheeran - Shape Of You (Instrumental)_2.wav'"
-            p = subprocess.Popen(shlex.split(cmd))
-
-    
     def bridging(self,fade_in_time=300,fade_out_time=300):
         ''' Default: 0.3sec fade in & out '''
         self.candSig = self.candSig.fade_in(fade_in_time).fade_out(fade_out_time)
@@ -69,9 +50,28 @@ class mash:
         print 'overlay'
 
 
+def volume_adjust(input_file):
+    import subprocess
+    import shlex
+    '''
+        Command format: " ffmpeg-normalize -v <input> "
+        e.g. "ffmpeg-normalize -v ../wav/'Shape Of You (Instrumental)_2.wav'"
+        File will be named by " normalized-<input> "
+        e.g. "normalized-Shape Of You (Instrumental)_2.wav"
+        '''
+    
+    FFMPEG_CMD = "ffmpeg-normalize"
+    #input_path = "../wav/"
+    #input_file = "'Ed Sheeran - Shape Of You (Instrumental)_4.wav'"
+    #cmd = FFMPEG_CMD + ' -v ' + input_path + input_file
+    cmd = FFMPEG_CMD + ' -v ' + input_file
+    #cmd = "ffmpeg-normalize -v ../wav/'Ed Sheeran - Shape Of You (Instrumental)_2.wav'"
+    p = subprocess.Popen(shlex.split(cmd))
+
+
 if __name__ == '__main__':
     song = mash('../../test.pgz', '../../test.pgz')
     song.output('ori.wav')
     song.time_stretch(2)
     song.output('./mix.wav')
-    #song.volume_adjust('ori.wav')
+    #volume_adjust('ori.wav')
