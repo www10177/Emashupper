a6db81d92e735480058f6a45838a34d8d4dfa441
Rist <www10177@gmail.com>
2017-12-20 17:02:00 +0800

    added unstretch_preprocessing.py

---
+++ utilities/unstretch_preprocessing.py
@@ -0,0 +1,30 @@
+import sys
+import os
+from sets import Set
+import sys
+sys.path.insert(0,'..')
+import lib.pre
+from  pandas import read_csv
+
+def find(dataframe, name):
+    for count, sname in enumerate(dataframe['song name']):
+        if  sname == name :
+            return dataframe['avg frame'][count]
+
+
+if len(sys.argv) != 3:
+    print( 'Usage: python preprocessing.py SONGFODLER PGZFOLDER')
+else :
+    inPath= sys.argv[1]
+    outPath = sys.argv[2]
+    csv = read_csv(os.path.join(outPath,'metadata.csv'))
+    i =0
+    for path,dir,files in os.walk(inPath):
+        for f in files:
+            if f.endswith('wav'):
+                # create preaudio instance and stretch to uniform frame size
+                instance  = lib.pre.PreAudio(os.path.join(path,f))
+                fTrim = filter(lambda x: not (x.isdigit() or x == '_'), instance.name) # to trim _1, _2, _3... off(number of song seg)_
+                instance.save(outPath)
+            print 'progress : ', i+1, ' of ', len(files)
+            i +=1
